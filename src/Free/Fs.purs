98a8e52a18e739f9dff01db583a4e237e4a9aca3
Risto Stevcev <risto1@gmail.com>
2017-06-29 15:04:05 +0200

    Added another free monad example

---
+++ src/Free/Fs.purs
@@ -0,0 +1,63 @@
+module Free.Fs where
+
+import Prelude (class Functor, Unit, pure, bind, id, unit, (<<<), (<>), (*>))
+import Node.Path (FilePath)
+import Node.Encoding (Encoding(UTF8))
+import Node.FS (FS)
+import Node.FS.Sync (readTextFile, writeTextFile)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Free (Free, foldFree, liftF)
+
+-- | Here's another example of a free monad DSL
+-- | Notice the pattern in free monads: it converts one abstract syntax tree into another. The DSL is
+-- | parameterized by `next` because we'll need to eventually provide an interpreter from `FileSystemF a -> Eff e a`,
+-- | or more generally a natural transformation from `f ~> g`, or in this case `FileSystemF ~> Eff e`.
+-- |
+-- | The `String -> next` looks a little unusual, since we know that readFileText will return a `String`,
+-- | But remember that it can't return a concrete type like String because the interpreter needs to be a natural
+-- | transformation (`f ~> g`), so we have to keep the AST general (using `next`). And it gives the free monad the
+-- | chance to pass the returned value to the next command using `bind`
+data FileSystemF next
+  = WriteFile Encoding FilePath String next
+  | ReadFile Encoding FilePath (String -> next)
+
+type FileSystem = Free FileSystemF
+
+instance functorFileSystemF :: Functor FileSystemF where
+  map f (WriteFile encoding filePath string next) = WriteFile encoding filePath string (f next)
+  -- | The `f <<< fNext` lets the free monad pass the value using `bind`
+  -- | ie, the `contents` in: `readFile UTF8 "foo.txt" >>= \contents`
+  map f (ReadFile encoding filePath fNext) = ReadFile encoding filePath (f <<< fNext)
+
+-- | These just provide some nicer functions for working with free DSLs
+writeFile :: Encoding -> FilePath -> String -> FileSystem Unit
+writeFile encoding filePath string = liftF (WriteFile encoding filePath string unit)
+
+readFile :: Encoding -> FilePath -> FileSystem String
+readFile encoding filePath = liftF (ReadFile encoding filePath id)
+
+
+-- | Here's a sample program
+-- | It just saves "hello, world!" to `foo.txt`, reads it, and then saves an extra "!" at the end
+echoHello :: FileSystem Unit
+echoHello = do
+  _ <- writeFile UTF8 "foo.txt" "hello, world!"
+  contents <- readFile UTF8 "foo.txt"
+  writeFile UTF8 "foo.txt" (contents <> "!")
+
+interpreter :: forall a e. FileSystemF a -> Eff (fs :: FS, exception :: EXCEPTION | e) a
+interpreter x = case x of
+  WriteFile encoding filePath string next -> do
+    _ <- writeTextFile encoding filePath string
+    pure next
+
+  ReadFile encoding filePath fNext -> do
+    contents <- readTextFile encoding filePath
+    -- | Remember the odd `String -> next` in the AST that was defined? this is why we needed it. The interpreter can
+    -- | pass the `contents` with `bind`
+    pure (fNext contents)
+
+main :: forall e. Eff (fs :: FS, exception :: EXCEPTION | e) Unit
+main = foldFree interpreter echoHello
--- src/Free/Monad.purs
+++ src/Free/Monad.purs
@@ -5,13 +5,13 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Free (Free, foldFree, liftF)
 
--- | Free monad provides a very flexible way to write a DSL. You can write a language that you can write multiple 
--- | different interpreters for. It's more powerful than free applicatives because as a monad it's capable of 
+-- | Free monad provides a very flexible way to write a DSL. You can write a language that you can write multiple
+-- | different interpreters for. It's more powerful than free applicatives because as a monad it's capable of
 -- | interpreting dynamic data at runtime. All IO/Eff can be rewritten using free monads.
 
--- | If you are trying to write a data structure that you want to interpret in more than one way, and that relies on 
--- | information provided at runtime, then you're probably looking for a free monad. Another use case is if you want 
--- | to build up a monadic context that you want to unwrap and interpret at a later time. An example of this would be 
+-- | If you are trying to write a data structure that you want to interpret in more than one way, and that relies on
+-- | information provided at runtime, then you're probably looking for a free monad. Another use case is if you want
+-- | to build up a monadic context that you want to unwrap and interpret at a later time. An example of this would be
 -- | for a unit testing framework.
 
 -- | The following example is a canonical and simple example of how it works. This was taken from one of Tel's lectures
@@ -44,12 +44,12 @@ echoTT = do
   case line of
     "quit" -> putLineTT "Bye!"  -- typing "quit" exits the REPL
     _ -> do
-      putLineTT line
+      _ <- putLineTT line
       echoTT
 
 -- | This is the actual interpreter. It takes the TeletypeF data structure and converts it to the type you want to use.
 -- | In this case the interpreter translates the TeletypeF DSL to the Eff monad.
--- | This shows the power and potential of the free monad. You can write many flavors of interpreters, and it can be 
+-- | This shows the power and potential of the free monad. You can write many flavors of interpreters, and it can be
 -- | used for mocking anything monadic such as database libs, AJAX, dom manipulations, IO, etc.
 interpreter :: forall a e. TeletypeF a -> Eff e a
 interpreter x = case x of
@@ -58,7 +58,7 @@ interpreter x = case x of
     line <- getLine
     pure (next line)
 
--- | This is the command that runs the interpreter. The foldFree function takes the interpreter/translator and the 
+-- | This is the command that runs the interpreter. The foldFree function takes the interpreter/translator and the
 -- | free monad, and it will unravel the free monad and translate it to Eff.
 echoEff :: forall e. Eff e Unit
 echoEff = foldFree interpreter echoTT
